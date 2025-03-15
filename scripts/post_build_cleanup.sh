ab41f55e466b4754929333beb006ecab6c2649b3
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-15 15:56:04 +0000

    adding .vscode back to scm

--- scripts/post_build_cleanup.sh
+++ scripts/post_build_cleanup.sh
@@ -2,6 +2,17 @@
 
 build_dir="../build"
 
+function remove_unnecessary_files() {
+    local list_of_unnecesssary_files=("start.mod.o" "stop.mod.o" ".Module.symvers.cmd" "modules.order.cmd" ".start.ko.cmd" "start.mod.cmd" "start.o.cmd" ".stop.mod.o.cmd" "modules.order" ".modules.order.cmd" "Module.symvers" "start.mod" "start.mod.c" ".start.mod.cmd" ".start.mod.o.cmd" ".start.o.cmd" ".stop.ko.cmd" "stop.mod" "stop.mod.c" ".stop.mod.cmd" ".stop.o.cmd")
+
+    for file in "${list_of_unnecesssary_files[@]}"; do
+        echo "Attempting to remove: $file"  # Debugging line
+        rm -f "$file"
+    done
+}
+
+remove_unnecessary_files
+
 function move_files_by_extension() {
     local extension="$1"
     local target_dir="$2"
@@ -14,18 +25,5 @@ function move_files_by_extension() {
     done
 }
 
-# move objs and kernel objs to respective dirs
 move_files_by_extension ".o" "${build_dir}/objs"
 move_files_by_extension ".ko" "${build_dir}/k_objs"
-
-# Remove unnecessary files
-function remove_unnecessary_files() {
-    local list_of_unnecesssary_files=(".Module.symvers.cmd" "modules.order.cmd" ".start.ko.cmd" "start.mod.cmd" "start.o.cmd" ".stop.mod.o.cmd" "modules.order" ".modules.order.cmd" "Module.symvers" "start.mod" "start.mod.c" ".start.mod.cmd" ".start.mod.o.cmd" ".start.o.cmd" ".stop.ko.cmd" "stop.mod" "stop.mod.c" ".stop.mod.cmd" ".stop.o.cmd")
-
-    for file in "${list_of_unnecesssary_files[@]}"; do
-        echo "Attempting to remove: $file"  # Debugging line
-        rm -f "$file"
-    done
-}
-
-remove_unnecessary_files
